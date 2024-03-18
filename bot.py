75d80492091c70e2b516df8ed93d879f14016608
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-03-18 11:06:04 +0300

    feat: contain API id and hash into environment variables

--- bot.py
+++ bot.py
@@ -1,10 +1,34 @@
 from telethon import TelegramClient, events, sync
+from dotenv import load_dotenv, dotenv_values
+import os
 
-# These example values won't work. You must get your own api_id and
-# api_hash from https://my.telegram.org, under API Development.
+load_dotenv()
 
-api_id = 12345
-api_hash = '0123456789abcdef0123456789abcdef'
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
 
-client = TelegramClient('session_name', api_id, api_hash)
-client.start()
+client = TelegramClient("session_name", api_id, api_hash)
+
+GREETING_POST = """
+🤝Благодарю за подписку!
+
+В качестве подарка составил для тебя список самых полезных каналов для саморазвития.
+   
+🧠[OLYMPIA](https://t.me/+-FOEdpd-h-I1ZWYy) - все тонкости мужской психологии, раскрытие женских приемов манипуляции.
+
+👁[Границы мужества](https://t.me/+WznZg97FDP80NjRi) - канал, вдохновляющий на преодоление жизненных вызовов и расширение границ собственных возможностей.
+"""
+
+
+async def main():
+    # Getting information about yourself
+    # CHAT_ID = -1002061276906
+    CHAT_ID = 749179973
+
+    await client.send_message(CHAT_ID, GREETING_POST, link_preview=False)
+
+
+with client:
+    client.loop.run_until_complete(main())
+
+# client.start()
